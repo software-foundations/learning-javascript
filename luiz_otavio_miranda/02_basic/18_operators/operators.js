f08f18d8c11a65f97d87ea3df66dc98ce8313247
brunoconde07 <brunoconde.ufpa@gmail.com>
2021-12-08 20:56:53 -0300

    class 21 (video 20)

---
+++ luiz_otavio_miranda/02_basic/18_operators/operators.js
@@ -0,0 +1,12 @@
+const a = 1
+const b = 100
+const c = 1
+
+console.log( a == c )
+console.log( a === c )
+console.log( a != b )
+console.log( a !== b )
+console.log( a < b )
+console.log( a <= b )
+console.log( b > a)
+console.log( b >= a)
