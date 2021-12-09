efb23483a3822fe46dc20c965b71f1d18862249e
brunoconde07 <brunoconde.ufpa@gmail.com>
2021-12-08 22:06:02 -0300

    [luiz] class 22(video 38)

---
+++ luiz_otavio_miranda/03_logic/01_operators_logic/operators_logic.js
@@ -0,0 +1,9 @@
+console.log(true && true)
+console.log(true || false)
+console.log(!false)
+
+const user = 'Bruno'
+const password = '123'
+const ableToLoggin = user === 'Bruno' && password === '123'
+
+console.log('ableToLoggin : ', ableToLoggin)
